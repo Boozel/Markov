41ba02d6c3e46ddb6809718ed14cfd6f40ce470b
GerrittDorland <gerrittdorland@gmail.com>
2015-05-24 13:55:35 -0700

    map and file read setup

--- main.cpp
+++ main.cpp
@@ -1,8 +1,28 @@
 #include <iostream>
+#include <map>
+#include <fstream>
+#include <string>
 
 using namespace std;
 
+bool buildMap(string, map< pair<string, string>, string> &);
+
 int main()
 {
-	cout << "Hello, World!" << endl;
+	map< pair<string, string>, string> suffix_map;
+
+	buildMap("fark.com", suffix_map);
+}
+
+bool buildMap(string filename, map< pair<string, string>, string> &in_suffix_map)
+{
+	ifstream fin;
+
+	fin.open(filename.c_str());
+	while(fin.good())
+	{
+		
+	}
+	fin.close();
+	return 1;
 }
